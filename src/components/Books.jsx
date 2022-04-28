79b82ac7db49c7b94cb73633a4259846f961c84f
Retky <jose.arredondo.a@outlook.com>
2022-04-28 17:44:27 -0500

    Add 'list' section & 'addBook' form

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Books = () => (
-  <div>Books</div>
+  <div>
+    <ul className="bookList">
+      <li>Book Element</li>
+    </ul>
+    <form className="addBook">
+      <h2>ADD NEW BOOK</h2>
+      <input type="text" name="title" placeholder="Book Title" />
+      <input type="text" name="author" placeholder="Book Author" />
+      <button className="addBtn" type="submit">Add Books</button>
+    </form>
+  </div>
 );
 
 export default Books;
