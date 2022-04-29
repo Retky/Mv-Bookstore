fc69f0f1f6e94608cef19e8d10f0f688a2e2287b
Retky <jose.arredondo.a@outlook.com>
2022-04-29 11:22:14 -0500

    Modify 'AddBook' separete form in new component

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import BookElement from './SingleBook';
+import AddBook from './AddBook';
 
 const Books = () => (
   <div>
     <ul className="bookList">
       <BookElement title="Book Title" author="Author Name" />
     </ul>
-    <form className="addBook">
-      <h2>ADD NEW BOOK</h2>
-      <input type="text" name="title" placeholder="Book Title" />
-      <input type="text" name="author" placeholder="Book Author" />
-      <button className="addBtn" type="submit">Add Books</button>
-    </form>
+    <AddBook />
   </div>
 );
 
